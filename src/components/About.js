e19f35e8813497114894537af23350da4fedaa35
saurav1207 <sauravkumar843114@gmail.com>
2023-10-28 19:15:42 +0530

    Updated code

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import './About.css';
 
 const About = () => {
   return (
-    <div>
-      <h2 className="text-center">About Page</h2>
+    <div className="about-page">
+      <h1 className="about-title">About URShop</h1>
+      <div className="about-content">
+        <p>
+          Welcome to URShop, your ultimate shopping destination! With URShop, you can discover
+          a wide range of products, find great deals, and shop with confidence.
+        </p>
+        <p>
+          Our mission is to provide you with a seamless and enjoyable shopping experience in an
+          intuitive and user-friendly platform. Whether you're looking for the latest trends,
+          unique finds, or everyday essentials, URShop has it all.
+        </p>
+        <p>
+          We understand the joy of finding that perfect item, whether you're upgrading your
+          wardrobe, decorating your home, or exploring new gadgets. That's why we've made URShop
+          convenient and reliable, so you can shop for what you love, when you want to.
+        </p>
+        <p>
+          Explore URShop now and embark on your shopping journey. Find what you desire, discover
+          new treasures, and make every purchase a delightful experience!
+        </p>
+      </div>
     </div>
   );
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,9 +6,12 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="logo">
-        <Link to="/">React-cart-app</Link>
+        <Link to="/">URShop</Link>
       </div>
       <ul className="navlinks">
+        <li className="navlink">
+          <Link to="/">Home</Link>
+        </li>
         <li className="navlink">
           <Link to="/cart">Cart</Link>
         </li>
